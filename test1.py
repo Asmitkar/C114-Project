a0d0f5e6712accff5fa5f0dcb50dc54a60728cd5
Asmitkar <89642589+Asmitkar@users.noreply.github.com>
2022-11-03 19:13:00 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,6 @@
+def my_function(fname):
+    print(fname + ' Refness')
+
+
+my_function('Chinmay')
+my_function('Vedant')
---
+++ test2.py
@@ -0,0 +1,6 @@
+a = 33
+b = 55
+if b > a:
+    print('b is greater than a')
+elif a > b:
+    print('a is greater than b')
